0965eb357f9289f0908bfedcc80fda44a7d020b5
pzh <854350704@qq.com>
2016-03-01 15:17:17 +0800

    添加登录界面

---
+++ app/src/main/java/com/pzh/common/AppConfig.java
@@ -0,0 +1,10 @@
+package com.pzh.common;
+
+import com.pzh.bean.UserBean;
+
+/**
+ * Created by pzh on 16/2/28.
+ */
+public class AppConfig {
+    public static UserBean userData;
+}
---
+++ app/src/main/java/com/pzh/ui/LoginInUI.java
@@ -0,0 +1,15 @@
+package com.pzh.ui;
+
+import android.os.Bundle;
+
+import com.pzh.common.BaseUI;
+
+/**
+ * Created by pzh on 16/2/28.
+ */
+public class LoginInUI extends BaseUI {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
